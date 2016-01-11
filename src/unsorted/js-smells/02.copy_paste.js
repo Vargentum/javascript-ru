3ea992566a72e261cbeb6bfb77afb1b494883225
Vargentum <vargentum@gmail.com>
2016-01-11 10:53:22 +0200

    js smells copy-paste chapter improvements

--- src/unsorted/js-smells/02.copy_paste.js
+++ src/unsorted/js-smells/02.copy_paste.js
@@ -1,19 +1,42 @@
 /*Old code*/
 
+function jsSmells02 () {
+    
+  var boxes = document.querySelectorAll('.Box');
 
-var boxes = document.querySelectorAll('.Box');
-
-[].forEach.call(boxes, function(element, index) {
-  element.innerText = "Box: " + index;
-  element.style.backgroundColor =
-    '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
-});
+  [].forEach.call(boxes, function(element, index) {
+    element.innerText = "Box: " + index;
+    element.style.backgroundColor =
+      '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+  });
 
 
-var circles = document.querySelectorAll(".Circle");
+  var circles = document.querySelectorAll(".Circle");
 
-[].forEach.call(circles, function(element, index) {
-  element.innerText = "Circle: " + index;
-  element.style.color =
-    '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
-});
+  [].forEach.call(circles, function(element, index) {
+    element.innerText = "Circle: " + index;
+    element.style.color =
+      '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+  });
+
+
+  /*Refactor*/
+
+  let getRandom = () => '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+  let $$ = selector => [].slice.call(document.querySelectorAll(selector || '*'))
+
+
+  let styleElems = (selector, caption) => {
+
+    $$(selector).forEach(elems, (elem, idx) => {
+      elem.innerText = `${caption}: ${idx}`
+      elem.style.color = getRandom
+    })
+  }
+
+
+  styleElems('.box', 'Box')
+  styleElems('.circle', 'Circle')
+
+}
+// jsSmells02()
